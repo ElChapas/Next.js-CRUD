fe6a2bc9f749630d030d58908511811ad572ec52
Oscar Chapa <tutoschapa@gmail.com>
2021-08-26 18:49:43 -0600

    Testeando deploy

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import SearchCard from "../components/Search/SearchCard";
 export default function Home({ isConnected }) {
     return (
         <div className="container">
-            <h1 className="title">Next.js CRUD</h1>
+            <h1 className="title">Next.js CRUD v1</h1>
             <div className="grid-container">
                 <FormCard />
                 <SearchCard />
